dcaa49dd8b4d89dba82971504a0414a59d6d318e
Anton Te <te.anton@gmail.com>
2018-09-26 19:18:30 -0700

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd server
+echo Entering directory \`server\'
+coddle
+echo Leaving directory \`server\'
+cd ..
+cd client
+echo Entering directory \`client\'
+coddle
+echo Leaving directory \`client\'
+cd ..
